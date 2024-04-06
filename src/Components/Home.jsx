0e36c2722f8d3b8b428bb00192531a575c16c029
Ritam Maity <ritammaity403@gmail.com>
2024-04-06 18:01:34 +0530

    some changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,45 +1,51 @@
 import React from "react";
 import "./Home.css";
+import Header from "./Header";
+import Footer from "./Footer";
 const Home = () => {
   return (
     <>
-      <div className="parent-container">
-        <div className="d-flex parts">
-          <div className="part1 d-flex flex-column justify-content-center align-items-center ">
-            <h3 className="text-white">MERN Stack Developer</h3>
-            <div className="h-50">
-              <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-              <lottie-player
-                src="https://lottie.host/e1aa1058-855b-4327-a7ea-3b419b220ee2/0rshikCjIi.json"
-                background="##FFFFFF"
-                speed="1"
-                loop
-                autoplay
-                direction="1"
-                mode="normal"
-              ></lottie-player>
+      <Header />
+      <div>
+        <div className="parent-container">
+          <div className="d-flex parts">
+            <div className="part1 d-flex flex-column justify-content-center align-items-center ">
+              <h3 className="text-white">MERN Stack Developer</h3>
+              <div className="h-50">
+                <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                <lottie-player
+                  src="https://lottie.host/e1aa1058-855b-4327-a7ea-3b419b220ee2/0rshikCjIi.json"
+                  background="##FFFFFF"
+                  speed="1"
+                  loop
+                  autoplay
+                  direction="1"
+                  mode="normal"
+                ></lottie-player>
+              </div>
             </div>
-          </div>
-          <div className="part2 d-flex flex-column justify-content-center align-items-center ">
-            <div className="h-50">
-              <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-              <lottie-player
-                src="https://lottie.host/a432eb8f-9fed-469d-bd1e-b15182d33fd7/URVeEKv5kj.json"
-                background="transparent"
-                speed="1"
-                loop
-                autoplay
-                direction="1"
-                mode="normal"
-              ></lottie-player>
+            <div className="part2 d-flex flex-column justify-content-center align-items-center ">
+              <div className="h-50">
+                <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                <lottie-player
+                  src="https://lottie.host/a432eb8f-9fed-469d-bd1e-b15182d33fd7/URVeEKv5kj.json"
+                  background="transparent"
+                  speed="1"
+                  loop
+                  autoplay
+                  direction="1"
+                  mode="normal"
+                ></lottie-player>
+              </div>
+              <h1 className="text-white"> WEB Developer</h1>
             </div>
-            <h1 className="text-white"> WEB Developer</h1>
+            {/* <div className="seprator">
+              <span>LANDING PAGE</span>
+            </div> */}
           </div>
-        </div>
-        <div className="seprator">
-          <span>LANDING PAGE</span>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
